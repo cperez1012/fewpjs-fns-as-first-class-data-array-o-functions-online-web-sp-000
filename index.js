2a947ad870cd0eeacd469b650192a7f94a468e24
Christian Perez <perezchristian1012@gmail.com>
2020-02-10 22:48:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function wakeDog(a, b) {
   console.log(`Wake ${a} the ${b}`);
+  return `Wake ${a} the ${b}`
+}
+
+function leashDog(a, b) {
+  console.log(`Leash ${a} the ${b}`);
+  `Leash ${a} the ${b}`
 }
